aadd8a4ad434b40b584d224c096d069c07cdc4ce
Michal <mcwynar04@gmail.com>
2023-01-27 16:33:30 +0100

    Pass + input

---
+++ Extra_topics/extra1.py
@@ -0,0 +1,19 @@
+# -----------------------------
+#           Lesson 1
+# -----------------------------
+# Pass + input
+
+# Pass tells Python to not do anything
+# You can add this to a function if you don't have code yet
+
+# Input is one way to get user input
+# Although it is very rarely used
+
+
+def really_important_function_name():
+    # without it we will get error (Python expected an indented block after function)
+    pass
+
+
+user_input = input('Press a button ')
+print(user_input)
