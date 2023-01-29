f1c21dd32e65bedb8e9d88d32acae0d42d807a0d
Michal <mcwynar04@gmail.com>
2023-01-29 16:42:15 +0100

    Surfaces

--- Asteroid Shooter/game_window.py
+++ Asteroid Shooter/game_window.py
@@ -6,14 +6,15 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 # set window title
 pygame.display.set_caption('Asteroid Shooter')
 
+# create a surface
+test_surf = pygame.Surface((400, 100))
+# we need to attach the surface to the display surface
+# both the display surface and the surface are black
+
+
 # keeps the code going
 while True:   # run forever -> keeps our game running
     # DO NOT RUN THE CODE UNTIL I TELL YOU
-
-    # The game loop
-    # The entire logic of our game will run in a while True loop
-    # Inside of it, we will check for any input, update all parts of the game and place graphics on the display surface
-    # At the end of the loop, we will show the display surface
 
     # 1. Input  -> events (mouse click, mouse movement, press of a button, controller or touchscreen)
     for event in pygame.event.get():
@@ -22,7 +23,9 @@ while True:   # run forever -> keeps our game running
             sys.exit()
 
     # 2. Updates
-    #nothing yet
+    display_surface.fill((15,140,122))
+    test_surf.fill((219,114,16))
+    display_surface.blit(test_surf, (0,0))
 
     # 3. Show the frame to the player / update the display surface
     pygame.display.update()
